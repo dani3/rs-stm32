4055b9de12ed3100e2c5f25552741063daa9967e
Daniel Mancebo Aldea <daniel.m.aldea@hotmail.com>
2021-03-24 19:28:11 +0100

    chore: added documentation, created dummy project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+pub use cortex_m_rt::entry;
+pub use panic_itm;
+
+#[entry]
+fn main() -> ! {
+    let _y;
+    let x = 42;
+    _y = x;
+
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
+}
